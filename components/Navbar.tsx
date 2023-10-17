9d63cd688aba99dc461b03fbfb6f01f2c296f813
chinclubi <c.soonue@gmail.com>
2023-10-17 21:34:35 +0700

    install : nextui

---
+++ components/Navbar.tsx
@@ -0,0 +1,28 @@
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Button,
+} from "@nextui-org/react";
+import Link from "next/link";
+
+export default function NavbarComponent() {
+  return (
+    <Navbar isBordered>
+      <NavbarBrand>
+        <p className="font-bold text-inherit">PACMAN</p>
+      </NavbarBrand>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+}
