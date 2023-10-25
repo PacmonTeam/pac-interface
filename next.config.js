9aae2aad6870845a11c7e8bf6854fc81dc4f1e53
chinclubi <c.soonue@gmail.com>
2023-10-25 22:20:09 +0700

    🚀 specific output path

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/project",
-        permanent: true,
-      },
-    ];
+  output: "export",
+  distDir: "_static",
+  images: {
+    unoptimized: true,
   },
 };
 
