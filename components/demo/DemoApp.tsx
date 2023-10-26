4bfa9021bbd72dc47fa4be9b685c3749621a43c1
Aikdanai Sidhikosol <aik>
2023-10-26 12:07:05 +0700

    feat: init demo page

---
+++ components/demo/DemoApp.tsx
@@ -0,0 +1,9 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+export default function DemoApp() {
+  return (
+    <ul className="divide-y divide-gray-600">
+      <ConnectButton />
+    </ul>
+  );
+}
--- config/url.ts
+++ config/url.ts
@@ -1,1 +1,26 @@
+import { Chain } from "@rainbow-me/rainbowkit";
+
 export const BASE_API = "https://o.pacmon.suijin.xyz/api";
+
+export const PACMON_CHAIN: Chain = {
+  id: 31337,
+  name: "Pacmon",
+  network: "pacmon",
+  iconUrl: "https://example.com/icon.svg",
+  iconBackground: "#fff",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Pacmon Ether",
+    symbol: "PAC",
+  },
+  rpcUrls: {
+    public: { http: ["https://o.pacmon.suijin.xyz/rpc"] },
+    default: { http: ["https://o.pacmon.suijin.xyz/rpc"] },
+  },
+  blockExplorers: {
+    default: { name: "PacmonScan", url: "https://etherscan.io" },
+    etherscan: { name: "PacmonScan", url: "https://etherscan.io" },
+  },
+  contracts: {},
+  testnet: true,
+};
