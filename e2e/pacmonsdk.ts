7b30f459a68c67750bd3f76276a6b1be268ae1b5
chimz <c.soonue@gmail.com>
2023-12-24 22:30:21 +0700

    move PacmonSDK to lib

--- e2e/pacmonsdk.ts
+++ e2e/pacmonsdk.ts
@@ -1,7 +1,7 @@
 import _ from "lodash";
 import { PacDemo__factory } from "./typechain";
 import { PacDemoInterface } from "./typechain/PacDemo";
-import axios, { Axios } from "axios";
+import { PacmonSDK } from "@/lib/PacmonSDK";
 
 interface Contracts {
   demo: string;
@@ -14,55 +14,6 @@ interface Contracts {
 
 interface Interfaces {
   demo: PacDemoInterface;
-}
-
-interface NodeInfoResp {
-  contracts: { sequence: number; name: string; address: string }[];
-  signers: { address: string }[];
-}
-
-export class PacmonSDK {
-  projectId: number;
-  nodeId?: number;
-  axiosClient: Axios;
-
-  constructor(projectId: number, api: string) {
-    this.projectId = projectId;
-    this.axiosClient = axios.create({
-      baseURL: api,
-      timeout: 300000,
-    });
-  }
-
-  async createNode() {
-    const resp = await this.axiosClient.post(`projects/deploy`, {
-      projectId: this.projectId,
-      nodeName: "Test Node",
-    });
-    return resp.data.id;
-  }
-
-  async deleteNode() {
-    const resp = await this.axiosClient.post(`nodes/delete`, {
-      nodeId: this.nodeId,
-    });
-  }
-
-  async getNodeInfo() {
-    const resp = await this.axiosClient.get<NodeInfoResp>(
-      `nodes/${this.nodeId}`
-    );
-    return resp.data;
-  }
-
-  async call(contractAddress: string, data: string, callerAddress: string) {
-    const resp = await this.axiosClient.post(`/nodes/call`, {
-      nodeId: this.nodeId,
-      contractAddress,
-      encodedData: data,
-      callerAddress,
-    });
-  }
 }
 
 export class PacmonDemoSDK extends PacmonSDK {
